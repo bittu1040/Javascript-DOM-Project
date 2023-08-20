555f2f08a864c745526564eb21679d09a5f30d54
debstepcoder <deblin.kumar@gmail.com>
2023-08-20 19:51:47 +0530

    bmi calc change

--- BMI-generator/index.js
+++ BMI-generator/index.js
@@ -15,11 +15,22 @@ document.getElementById('calculate').addEventListener('click', (e) => {
 
     if (height === '' || height < 0 || isNaN(height)) {
         results.innerHTML = `Please give a valid height ${height}`;
+        window.alert("empty value")
     } else if (weight === '' || weight < 0 || isNaN(weight)) {
         results.innerHTML = `Please give a valid weight ${weight}`;
+        window.alert("empty value")
     } else {
         const bmi = (weight / ((height * height) / 10000)).toFixed(2);
         results.innerHTML = `<span>${bmi}</span>`;
     }
+});
+
+document.getElementById('reset').addEventListener('click', (e) => {
+    e.preventDefault();
+    document.getElementById("height").value="";
+    document.getElementById("weight").value="";
+
+        results.innerHTML = 0
+    
 });
 
