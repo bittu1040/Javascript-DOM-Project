c123fff186e2d896ce717a345cce90d6420afddf
Bittu <bittu1040@gmail.com>
2023-09-14 11:30:02 +0530

    stop and reset button

--- countdown timer/index.js
+++ countdown timer/index.js
@@ -5,31 +5,37 @@ var resetButton = document.getElementById("reset");
 var minuteInput = document.getElementById("minute");
 var secondInput = document.getElementById("second");
 
-resetButton.addEventListener('click', () => {
-    minute.innerText = "00";
-    second.innerText = "00";
-})
-
 
 startButton.addEventListener('click', () => {
     let minute = minuteInput.textContent;
     let second = secondInput.textContent;
 
-    startButton.disabled = true;
-    stopButton.disabled = false;
-
     startInterval = setInterval(() => {
+        if (minute === 0 && second === 0) {
+            // clearInterval(startInterval);
+            startButton.disabled = true;
+            stopButton.disabled = false;
+            return;
+        }
+
         if (minute > 0) {
+            startButton.disabled = true;
+            stopButton.disabled = false;
             minute = minute - 1;
             console.log(minute)
             minuteInput.textContent= minute;        }
     }, 1000);
 })
 
-stopButton.addEventListener('click', ()=>{
+stopButton.addEventListener('click', () => {
     clearInterval(startInterval);
     startButton.disabled = false;
     stopButton.disabled = true;
+})
+
+resetButton.addEventListener('click', () => {
+    minute.innerText = "00";
+    second.innerText = "00";
 })
 
 
