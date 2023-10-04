37669012300afbc220344c539bd6b3d86f99722c
Bittu <bittu1040@gmail.com>
2023-10-04 06:14:00 +0530

    basic stopwatch implemented

--- stopwatch/index.js
+++ stopwatch/index.js
@@ -4,41 +4,20 @@ let seconds = 0;
 let minutes = 0;
 let hours = 0;
 
-function startStop() {
-    if (isRunning) {
-        clearInterval(timer);
-        document.getElementById("startStop").textContent = "Start";
-    } else {
-        timer = setInterval(updateDisplay, 1000);
-        document.getElementById("startStop").textContent = "Stop";
-    }
-    isRunning = !isRunning;
-}
 
-function reset() {
-    clearInterval(timer);
-    isRunning = false;
-    seconds = 0;
-    minutes = 0;
-    hours = 0;
-    updateDisplay();
-    document.getElementById("startStop").textContent = "Start";
-}
-
-function updateDisplay() {
-    seconds++;
-    if (seconds === 60) {
-        seconds = 0;
-        minutes++;
-        if (minutes === 60) {
-            minutes = 0;
-            hours++;
+document.getElementById("startStop").addEventListener("click", function(){
+    setInterval(() => {
+        seconds++;
+        if(seconds === 60){
+            seconds = 0;
+            minutes++;
+            if(minutes === 60){
+                minutes = 0;
+                hours++;
+            }
         }
-    }
+        const display=hours.toString().padStart(2, '0') + ':' + minutes.toString().padStart(2, '0')+':' + seconds.toString().padStart(2, '0');
+        document.querySelector(".display").textContent = display;
 
-    const display = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
-    document.querySelector(".display").textContent = display;
-}
-
-document.getElementById("startStop").addEventListener("click", startStop);
-document.getElementById("reset").addEventListener("click", reset);
+    }, 1000);
+});
