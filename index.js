cf7fe7365a040b98cd7ef4a60c551e4f0bb43234
Bittu <bittu1040@gmail.com>
2024-03-29 10:41:01 +0530

    added search functionality

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 console.log("js loaded");
 console.log("new branch created");
+
+document.getElementById("searchInput").addEventListener('input', function() {
+    var searchValue = this.value.toLowerCase();
+    var projectLinks = document.querySelectorAll('.project-link');
+    console.log(searchValue)
+    console.log(projectLinks)
+    projectLinks.forEach(function(link) {
+      var projectTitle = link.querySelector('a').textContent.toLowerCase();
+      if (projectTitle.includes(searchValue)) {
+        link.style.display = 'block';
+      } else {
+        link.style.display = 'none';
+      }
+    });
+  });
