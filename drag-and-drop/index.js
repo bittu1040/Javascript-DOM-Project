58e3488ea889485831a81a7545a4b25ab14ffc2b
Bittu <bittu1040@gmail.com>
2025-01-24 01:38:01 +0530

    drag and drop, added one more item

--- drag-and-drop/index.js
+++ drag-and-drop/index.js
@@ -2,7 +2,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const items = [
         { id: 1, name: 'Item 1' },
         { id: 2, name: 'Item 2' },
-        { id: 3, name: 'Item 3' }
+        { id: 3, name: 'Item 3' },
+        { id: 4, name: 'Item 4 '}
     ];
 
     const container1 = document.querySelector('.container-1');
